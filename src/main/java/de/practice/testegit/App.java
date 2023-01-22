16c242d6dd268007e37ab9de6088f9f68bab04fc
SchiggyWara <gabriel.beussel94@gmx.de>
2023-01-22 23:15:12 +0100

    first commit

---
+++ src/main/java/de/practice/testegit/App.java
@@ -0,0 +1,13 @@
+package de.practice.testegit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
